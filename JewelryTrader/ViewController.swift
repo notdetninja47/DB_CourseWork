ef71567cc0aff8f7b3c88f70e965fcf68a224a60
notdetninja47 <slupdo09@gmail.com>
2016-11-27 14:52:10 +0200

    Initial Commit

---
+++ JewelryTrader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JewelryTrader
+//
+//  Created by Daniel Slupskiy on 27.11.16.
+//  Copyright © 2016 Daniel Slupskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
